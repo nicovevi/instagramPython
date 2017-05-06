98e65644d743362d869c633663c5e06cce1c167e
Centro Ático <centroatico@202C11E21168963.local>
2017-05-06 13:02:54 -0500

    segundo commit

--- instagram/instagramapp/urls.py
+++ instagram/instagramapp/urls.py
@@ -1,7 +1,14 @@
 from django.conf.urls import url
 
+
+
+from . import views
+
+
 urlpatterns = [
-        url(r'^registrar/', veiws.registrar ),
+        #url(r'^registrar/', views.registrar ),
+        url(r'^$', views.index, name = 'index'),
+        url(r'^$', views.index, name = 'login'),
 
 
 ]
