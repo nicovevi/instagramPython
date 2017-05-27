ade3be8c0f8a3757bea60eae91d9d4208fdbdb87
Centro Ático <centroatico@202C11E21168963.local>
2017-05-27 12:48:56 -0500

    quinto commit

--- instagram/instagramapp/urls.py
+++ instagram/instagramapp/urls.py
@@ -8,8 +8,8 @@ from . import views
 urlpatterns = [
         #url(r'^registrar/', views.registrar ),
         url(r'^$', views.index, name = 'index'),
-        url(r'^login/$', views.index, name = 'login'),
-        url(r'^home/$', views.index, name = 'home'),
+        url(r'^login/$', views.login, name = 'login'),
+        url(r'^home/$', views.home, name = 'home'),
         url(r'^crear_usuario/$', views.crear_usuario, name = 'crear_usuario'),
 
 
--- instagram/instagramapp/views.py
+++ instagram/instagramapp/views.py
@@ -1,18 +1,20 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from instagramapp.models import *
 # Create your views here.
 def index (request):
     return render(request,'Registro.html')
 
 def login(request):
-    return render(request,'Log-in.html')
+    return render(request,'Login.html')
 
 def home(request):
     return render(request,'home.html')
 
 def crear_usuario(request):
-    print(request.POST['email'])
-    print(request.POST['name'])
-    print(request.POST['username'])
-    print(request.POST['password'])
-    return render(request,'Log-in.html'
+
+    user = User.objects.create_user (username = request.POST['username'], email = request.POST['email'], first_name = request.POST['name'], password = request.POST['password'])
+    myuser = MiUsuario (usuario = user)
+    print(user)
+    print(user.password)
+    return redirect ('login')
