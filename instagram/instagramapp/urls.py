c96f47fbff88b1403015a5a3fc5f47f6f17d7885
Centro Ático <centroatico@202C11E21168963.local>
2017-06-03 13:06:36 -0500

    sexto commit

--- instagram/instagramapp/urls.py
+++ instagram/instagramapp/urls.py
@@ -1,16 +1,19 @@
 from django.conf.urls import url
+from django.contrib.auth import views as auth_views
 
 
-
-from . import views
+from.import views
 
 
 urlpatterns = [
         #url(r'^registrar/', views.registrar ),
         url(r'^$', views.index, name = 'index'),
-        url(r'^login/$', views.login, name = 'login'),
         url(r'^home/$', views.home, name = 'home'),
+        url(r'^profile/$', views.profile, name = 'profile'),
+    #    url(r'^login/$', views.login, name = 'login'),
         url(r'^crear_usuario/$', views.crear_usuario, name = 'crear_usuario'),
+        url(r'^login/$', auth_views.LoginView.as_view(template_name='Login.html' ,redirect_authenticated_user= True), name = 'login'),
+        url(r'^logout/$', auth_views.LogoutView.as_view(template_name='Registro.html'), name = 'logout')
 
 
 
--- instagram/instagramapp/views.py
+++ instagram/instagramapp/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from instagramapp.models import *
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 def index (request):
     return render(request,'Registro.html')
@@ -8,6 +9,7 @@ def index (request):
 def login(request):
     return render(request,'Login.html')
 
+@login_required
 def home(request):
     return render(request,'home.html')
 
@@ -18,3 +20,6 @@ def crear_usuario(request):
     print(user)
     print(user.password)
     return redirect ('login')
+
+def profile(request):
+    return render(request,'profile.html')
